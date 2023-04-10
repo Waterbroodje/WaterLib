8efd9ea1342f6a1210cd707bf370bd73f92a522b
timokorpershoek <wortelschaaf.info@gmail.com>
2023-04-10 21:16:19 +0200

    Added some classes

---
+++ src/main/java/me/waterbroodje/waterlib/data/mysql/PlayerProfile.java
@@ -0,0 +1,36 @@
+package me.waterbroodje.waterlib.data.mysql;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerProfile {
+
+    private final Player player;
+    private final Map<String, Object> data = new HashMap<>();
+
+    public PlayerProfile(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void addData(String key, Object value) {
+        data.put(key, value);
+    }
+
+    public Object getData(String key) {
+        return data.get(key);
+    }
+
+    public void removeData(String key) {
+        data.remove(key);
+    }
+}
